61d358ac98aaea64f06a5447c2a619b36c3d79ab
dalejovis <contato.jovis@gmail.com>
2023-04-03 21:01:37 -0300

    Adicionando projeto-react-js

---
+++ P4/Tec. para Front End III/Prova01 - Quiz ReactJS/quiz-react-js/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Quiz de Front-End</h1>
+
+      <h2>Projeto de avaliação da disciplina de Tecnologias para Front-End III</h2>
+      
+      <div className='perguntas'>
+        <h3>Pergunta Nº 1:</h3>
+
+        <ul>
+          <li>Opção 1</li>
+          <li>Opção 2</li>
+          <li>Opção 3</li>
+          <li>Opção 4</li>
+        </ul>
+
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
